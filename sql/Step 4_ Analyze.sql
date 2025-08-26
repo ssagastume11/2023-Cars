f0429ae58a1357b3f9dd244a089243ec39b3bbda
ssagastume11 <sagassergio123@gmail.com>
2025-08-26 09:12:05 -0400

    BigQuery SQL queries

--- sql/Step 4_ Analyze.sql
+++ sql/Step 4_ Analyze.sql
@@ -1,7 +1,8 @@
 -- Objective:
--- Interpret processed sales data to uncover significant trends and patterns during the first half of 2025. This includes identifying the highest and lowest-performing months in both sales revenue and order volume. The results will help the Pakastani company understand seasonality, customer behavior, and potential marketing impacts.
+-- Explore relationships between car attributes, identify market trends, and uncover patterns that explain pricing, performance, and consumer preferences. 
 
--- Key Questions:
--- Which month had the highest total sales?
--- Which month had the lowest total sales?
--- Are there any noticeable spikes or drops in order volume or quantity sold?
+-- Key Actions:
+-- * Perform descriptive statisitics to summarize key metrics (price, horsepower, mileage, ratings).
+-- * Compare market positioning by body type, fuel type, and transimission.
+-- * Analyze relationships between price and performance metrics.
+-- * Identify top-selling car models and factors influencing their success.
