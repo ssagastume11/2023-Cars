5b6beb75761502eaed14839ec3addca57c258dfd
ssagastume11 <sagassergio123@gmail.com>
2025-08-26 09:09:07 -0400

    BigQuery SQL queries

---
+++ sql/Step 4_ Analyze.sql
@@ -0,0 +1,7 @@
+-- Objective:
+-- Interpret processed sales data to uncover significant trends and patterns during the first half of 2025. This includes identifying the highest and lowest-performing months in both sales revenue and order volume. The results will help the Pakastani company understand seasonality, customer behavior, and potential marketing impacts.
+
+-- Key Questions:
+-- Which month had the highest total sales?
+-- Which month had the lowest total sales?
+-- Are there any noticeable spikes or drops in order volume or quantity sold?
