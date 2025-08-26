485673f1e2f701c30ad956746a19f61e6d9411f5
ssagastume11 <sagassergio123@gmail.com>
2025-08-26 09:05:27 -0400

    BigQuery SQL queries

---
+++ sql/Step 1_ Ask.sql
@@ -0,0 +1,3 @@
+Business Task:
+
+The goal of this analysis is to identify monthly sales trends from January to June 2025 using transactional data from a Pakistan-based e-commerce store specializing in unique gifts for any occasion. By examining how sales fluctuates over time, the company can identify patterns, evaluate marketing effectiveness, and prepare for future demand.
