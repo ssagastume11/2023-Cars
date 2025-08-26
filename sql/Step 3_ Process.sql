17e3715d58c4632569f822fd6204ce612c3b68f6
ssagastume11 <sagassergio123@gmail.com>
2025-08-26 09:08:27 -0400

    BigQuery SQL queries

---
+++ sql/Step 3_ Process.sql
@@ -0,0 +1,10 @@
+-- Objective: 
+
+-- Extract and aggregate relevant data to begin identifying monthly sales trends. This involves grouping transactions by month and calculating total sales and total quantity sold. The processed output will provide a clear view of sales activity over time, which is essential for trend analysis.
+
+-- Key Actions:
+
+-- * Group transactions by order_month.
+-- * Caluclate total sales revenue per month.
+-- * Calculate total quantity of items sold per month.
+-- * Sort results chronologically to support visualization and trend spotting.
