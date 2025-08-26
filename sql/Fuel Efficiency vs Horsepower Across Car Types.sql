3485225dc56b647bd00c4ed2d3fe76d051c48296
ssagastume11 <sagassergio123@gmail.com>
2025-08-26 14:28:58 -0400

    BigQuery SQL queries

--- sql/Fuel Efficiency vs Horsepower Across Car Types.sql
+++ sql/Fuel Efficiency vs Horsepower Across Car Types.sql
@@ -1,1 +1,14 @@
-
+SELECT 
+  Car_Make,
+  AVG(Horsepower) AS avg_horsepower,
+  AVG(Mileage_MPG) AS avg_mileage_mpg,
+  COUNT(*) AS num_models
+FROM 
+  `plenary-ability-463920-b3.cars_2023.cars_data_processed`
+WHERE 
+  Horsepower IS NOT NULL
+  AND Mileage_MPG IS NOT NULL
+GROUP BY 
+  Car_Make
+ORDER BY 
+  avg_mileage_mpg DESC;
