939387a245359d84c23595584802a1765d7d82c2
ssagastume11 <sagassergio123@gmail.com>
2025-08-26 09:07:18 -0400

    BigQuery SQL queries

---
+++ sql/Step 2_ Prepare.sql
@@ -0,0 +1,12 @@
+Objective:
+Ensure the dataset is clean, consistent, and ready for analysis. This involves inspecting the structure, handling missing values, and creating a time-based column to support trend analysis.
+
+Key Actions:
+
+Review the schema and preview the data
+
+Identify and handle any missing values
+
+Confirm date formatting
+
+Derive a order_month field to analyze monthly trends
